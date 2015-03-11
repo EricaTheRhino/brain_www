b06ca60a653fdac0fbaecaba171ce3fc4792a4cd
Andrew Milsted <andy@bluerhinos.co.uk>
2015-03-11 09:11:29 +0000

    Added eye address

--- status/settings.php
+++ status/settings.php
@@ -1,5 +1,4 @@
 <?php
 	
-$config['eye']['left'] = array("url"=>"http://left-eye.ericatherhino.test:8182/image/current", "cache"=>"/tmp/erica/eyes/left");
-$config['eye']['right'] = array("url"=>"http://right-eye.ericatherhino.test:8182/image/current", "cache"=>"/tmp/erica/eyes/right");
-$config['eye']['left'] = array("url"=>"http://www.pocruises.com/webcams/britannia.jpg", "cache"=>"/tmp/erica/eyes/left");
+$config['eye']['left'] = array("url"=>"http://left-eye.etr:8182/image/current", "cache"=>"/tmp/erica/eyes/left");
+$config['eye']['right'] = array("url"=>"http://right-eye.etr:8182/image/current", "cache"=>"/tmp/erica/eyes/right");
