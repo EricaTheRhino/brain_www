e8224016d1727d74425a1c0e8de660a49c5285ad
Andrew Milsted <a.j.milsted@soton.ac.uk>
2015-03-11 14:58:22 +0000

    added ramdisk and events

--- status/proxy.php
+++ status/proxy.php
@@ -3,12 +3,12 @@
 include('settings.php');
 
 #Proxy has short fuse;
-ini_set('default_socket_timeout', 1);
+ini_set('default_socket_timeout', $config['eye']['cache']*4);
 
 if(isset($_REQUEST['eye']) && isset($config['eye'][$_REQUEST['eye']])){
 	$eye = $config['eye'][$_REQUEST['eye']];
 	
-	if(!file_exists($eye['cache']) || (time() - filemtime($eye['cache']))>5){
+	if(!file_exists($eye['cache']) || (time() - filemtime($eye['cache']))>$config['eye']['cache']){
 		
 		if(!file_exists($eye['cache']))
 			@mkdir(dirname($eye['cache']),0777,true);
@@ -30,7 +30,19 @@ if(isset($_REQUEST['eye']) && isset($config['eye'][$_REQUEST['eye']])){
 
 if(isset($_REQUEST['task']) && $_REQUEST['task'] == $_REQUEST['task']){
 	
-	echo "OK";
+	$entityBody = file_get_contents('php://input');
+	$url = 'URL';
+	$data = array('field1' => 'value', 'field2' => 'value');
+	$options = array(
+	        'http' => array(
+	        'method'  => 'POST',
+	        'content' => http_build_query($entityBody),
+	    )
+	);
+		$context  = stream_context_create($options);
+	$result = file_get_contents($config['brain']['events']['url'], false, $context);
+	echo "OK;";
+	echo $result;
 
 	
 }
--- status/settings.php
+++ status/settings.php
@@ -1,4 +1,8 @@
 <?php
 	
-$config['eye']['left'] = array("url"=>"http://left-eye.etr:8182/image/current", "cache"=>"/tmp/erica/eyes/left");
-$config['eye']['right'] = array("url"=>"http://right-eye.etr:8182/image/current", "cache"=>"/tmp/erica/eyes/right");
+$config['eye']['left'] = array("url"=>"http://right-eye.etr:8182/image/current", "cache"=>"/mnt/ramdisk/erica/eyes/left");
+$config['eye']['right'] = array("url"=>"http://right-eye.etr:8182/image/current", "cache"=>"/mnt/ramdisk/erica/eyes/right");
+$config['eye']['cache'] = 1;
+
+
+$config['brain']['events'] = array("url"=>"http://brain.etr/events/");
