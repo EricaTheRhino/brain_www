0edf5cc2f99533cc061f8bb2328d95ff737677f7
Andrew Milsted <andrew@bluerhinos.co.uk>
2015-03-12 17:45:34 +0000

    updated left

--- status/settings.php
+++ status/settings.php
@@ -1,6 +1,6 @@
 <?php
 	
-$config['eye']['left'] = array("url"=>"http://right-eye.etr:8182/image/current", "cache"=>"/mnt/ramdisk/erica/eyes/left");
+$config['eye']['left'] = array("url"=>"http://left-eye.etr:8182/image/current", "cache"=>"/mnt/ramdisk/erica/eyes/left");
 $config['eye']['right'] = array("url"=>"http://right-eye.etr:8182/image/current", "cache"=>"/mnt/ramdisk/erica/eyes/right");
 $config['eye']['cache'] = 1;
 
